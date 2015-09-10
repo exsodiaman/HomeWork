bc92d358dc098c8d06af94b2724ce9d25763ae79
exsodiaman <exsodiaman@gmail.com>
2015-09-10 11:36:25 +0700

    Homework 02

---
+++ HomeWork_2 13550128/Assets/Script/Light.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Light : MonoBehaviour {
+
+	public GameObject LightA;
+	bool IsKill = false;
+	public GameObject[] Prison = new GameObject[16];
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			StartCoroutine(OffLight());
+	}
+
+	IEnumerator OffLight(){
+		LightA.SetActive(false);
+		yield return new WaitForSeconds (0.5f);
+		LightA.SetActive(true);
+		yield return new WaitForSeconds (0.5f);
+		LightA.SetActive(false);
+		yield return new WaitForSeconds (0.5f);
+		LightA.SetActive(true);
+		yield return new WaitForSeconds (1f);
+	}
+
+	IEnumerator Hurt(){
+		Prison[0].transform.eulerAngles = new Vector3(0,-5,0);
+		yield return new WaitForSeconds (0.25f);
+		Prison[0].transform.eulerAngles = new Vector3(0,-10,0);
+		yield return new WaitForSeconds (0.25f);
+		Prison[0].transform.eulerAngles = new Vector3(0,-15,0);
+		yield return new WaitForSeconds (0.25f);
+		Prison[0].transform.eulerAngles = new Vector3(0,-20,0);
+		yield return new WaitForSeconds (0.25f);
+		Prison[0].transform.eulerAngles = new Vector3(0,-25,0);
+		yield return new WaitForSeconds (0.25f);
+		Prison[0].transform.eulerAngles = new Vector3(0,-30,0);
+		yield return new WaitForSeconds (2f);
+		Kill();
+	}
+
+	void Kill(){
+		for(int i = 0;i<16;i++){
+			Prison[i].GetComponent<Rigidbody>().isKinematic=false;
+		}
+	}
+
+	public void ClickK(){
+		StartCoroutine(Hurt ());
+	}
+
+}
